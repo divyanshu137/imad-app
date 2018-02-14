907b49a85fbad94403e3abd18a5e1bfaa04fa711
Divyanshu Malhotra <divyanshu137@gmail.com>
2018-02-14 12:55:24 +0530

    changes server files

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('page-one', function(req, res) {
+app.get('/page-one', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
 });
 
-app.get('page-two', function(req, res) {
+app.get('/page-two', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-two.html'));
 });
 
-app.get('page-three', function(req, res) {
+app.get('/page-three', function(req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-three.html'));
 });
 
