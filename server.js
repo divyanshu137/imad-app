d9160036755770763c6fcac92cde01e913218b13
Divyanshu Malhotra <divyanshu137@gmail.com>
2018-02-28 16:44:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'divyanshu137',
+    database: 'divyanshu137',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
+
+
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    //make a select request
+    //return the response with the results
+    pool.query('SELECT * FROM test', function(err, result) {
+        if(err){
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
 });
 
 app.get('/page-one', function(req, res) {
